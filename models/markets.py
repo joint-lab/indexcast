61521c01a151522c96f76ebfa12f407c69cefadb
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2025-06-16 21:30:36 -0400

    Improve naming convention

--- models/markets.py
+++ models/markets.py
@@ -15,15 +15,19 @@ class Market(SQLModel, table=True):
 
     __tablename__ = "markets"
 
+    # identifiers
     id: str = Field(primary_key=True)
     creator_id: str
-    created_time: datetime = Field(default_factory=lambda: datetime.now(UTC))
     url: str
+    # content
     question: str
     description: str | None = None
-    close_time: datetime | None = None
+    # trading summaries
     probability: float | None = None
     volume: float | None = Field(default=0.0)
     is_resolved: bool = Field(default=False)
     resolution: str | None = None
-    last_updated: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    # timestamps
+    created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    closed_at: datetime | None = None
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
