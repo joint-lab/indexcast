9c6271186822c1e481b87ee09144ca728d2b4247
Jean-Gabriel Young <jean.gabriel.young@gmail.com>
2025-06-18 16:52:40 -0400

    Add update job

--- pipelines/definitions.py
+++ pipelines/definitions.py
@@ -12,11 +12,16 @@ from pipelines.resources.api import manifold_api_resource
 from pipelines.resources.db import sqlite_db_resource
 
 markets_assets = dg.load_assets_from_modules([markets])
+market_job = dg.define_asset_job(
+            name="update_markets_job",
+            selection="manifold_markets+"
+)
 
 defs = dg.Definitions(
     assets=[*markets_assets],
     resources={
         "database_engine": sqlite_db_resource,
         "manifold_client": manifold_api_resource
-    }
+    },
+    jobs=[market_job]
 )
--- pipelines/jobs/update_markets.py
+++ pipelines/jobs/update_markets.py
@@ -7,24 +7,14 @@ Authors:
 """
 import dagster as dg
 
-from pipelines.assets.market_labels import market_labels
-from pipelines.assets.markets import markets
-from pipelines.resources.api import manifold_api_resource
-from pipelines.resources.db import sqlite_db_resource
+# Use the Definitions object to get the job and resources
+update_markets_job = dg.define_asset_job(
+    name="update_markets_job",
+    selection="manifold_markets+"
+)
 
-
-@dg.job(resource_defs={
-    "sqlite_db_resource": sqlite_db_resource,
-    "manifold_api_resource": manifold_api_resource,
-})
-def update_markets_job():
-    """Job to update markets and market labels."""
-    labels = market_labels(markets())
-    # do something with labels, e.g., store them in the database
-    return labels
-
-# Schedule: run hourly
 update_markets_schedule = dg.ScheduleDefinition(
     job=update_markets_job,
     cron_schedule="0 * * * *",  # every hour
+    execution_timezone="UTC",
 )
