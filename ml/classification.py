eff9d83b2210e2a7793c743ca998639cefe30089
erikarnold <ewarnold@uvm.edu>
2025-07-08 21:50:40 -0500

    improved naming

--- ml/classification.py
+++ ml/classification.py
@@ -19,12 +19,17 @@ class H5N1Classifier:
     def __init__(self):
         """Initialize H5N1Classifier."""
         self.model = SentenceTransformer('Alibaba-NLP/gte-base-en-v1.5', trust_remote_code=True)
-        # classifier trained from the market_classification_summary.ipynb in notebooks directory
+        # initial classifier trained from the market_classification_summary.ipynb
+        # in notebooks directory this classifier gets all markets that could be relating to H5N1
         base_dir = path.dirname(path.abspath(__file__))
-        joblib_path = path.join(base_dir, "binary", "classifier_pipeline.joblib")
-        self.classifier = joblib.load(joblib_path)
-        joblib_path2 = path.join(base_dir, "binary", "second_classifier_pipeline.joblib")
-        self.second_classifier = joblib.load(joblib_path2)
+        path_to_initial_classifier = path.join(base_dir, "binary",
+                                               "initial_classifier_pipeline.joblib")
+        self.initial_classifier = joblib.load(path_to_initial_classifier)
+        # second classifier trained from the second_classifier.ipynb in notebooks directory
+        # this classifier prunes the results of the original for relevance to H5N1
+        path_to_relevance_classifier = path.join(base_dir, "binary",
+                                                 "relevance_classifier_pipeline.joblib")
+        self.relevance_classifier = joblib.load(path_to_relevance_classifier)
 
 
     def predict(self, market: Market) -> bool:
